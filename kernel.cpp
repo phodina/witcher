67b03cb742015315ab3a3b1770fc0425e03b0457
Petr Hodina <hodinapetr46@gmail.com>
2018-05-15 22:50:49 +0200

    Improve printf (stateful, new line, line wrap, screen clear).

--- kernel.cpp
+++ kernel.cpp
@@ -3,16 +3,46 @@
 
 void printf(char* str) {
 
-  uint16_t* VideoMemory = (uint16_t*)0xb8000;
+  static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+  static uint8_t x=0, y=0;
 
   for(uint16_t i = 0; str[i]!= '\0'; ++i) {
-    VideoMemory[i] = (VideoMemory[i] & 0xff00) | str[i];
+    switch (str[i]) {
+    case '\n':
+      y++;
+      x = 0;
+      break;
+    default:
+      VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xff00) | str[i];
+      x++;
+      break;
+
+    }
+    if (x >= 80) {
+      y++;
+      x = 0;
+    }
+
+    if (y >= 25) {
+
+      for (y=0; y<25; y++) {
+	for (x=0;x<80;x++) {
+	  VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xff00) | ' ';
+	}
+      }
+      
+      x = 0;
+      y = 0;
+    }
   }
 }
 
 extern "C" void kernelMain (void *multiboot_structure, uint32_t magic_number) {
 
-  printf("Hello Witcher!");
+  printf("Hello Witcher!\n");
+
+  printf("Hello Witcher!\n");
 
   GlobalDescriptorTable gdt;
   
