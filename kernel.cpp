1b8f1aa5333cdca8a62afc60e18f3bbbdc79817e
Petr Hodina <hodinapetr46@gmail.com>
2018-05-15 18:44:22 +0200

    Implement printf.

--- kernel.cpp
+++ kernel.cpp
@@ -1,4 +1,13 @@
 
+void printf(char* str) {
+
+  unsigned short* VideoMemory = (unsigned short*)0xb8000;
+
+  for(int i = 0; str[i]!= '\0'; ++i) {
+    VideoMemory[i] = (VideoMemory[i] & 0xff00) | str[i];
+  }
+}
+
 void kernelMain (void *multiboot_structure, unsigned int magic_number) {
 
   printf("Hello Witcher!");
